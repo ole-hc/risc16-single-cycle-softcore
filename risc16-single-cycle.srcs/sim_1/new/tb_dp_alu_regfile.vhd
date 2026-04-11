148978681760ebb1dce062fa7eddc1a40e8b9053
ole-hc <hofmann.ole.eibach@gmail.com>
2026-04-11 15:26:12 +0200

    Add alu_regfile datapath with testbench

---
+++ risc16-single-cycle.srcs/sim_1/new/tb_dp_alu_regfile.vhd
@@ -0,0 +1,86 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/11/2026 02:34:10 PM
+-- Design Name: 
+-- Module Name: tb_dp_alu_regfile - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_dp_alu_regfile is
+--  Port ( );
+end tb_dp_alu_regfile;
+
+architecture Behavioral of tb_dp_alu_regfile is
+
+component dp_alu_regfile is
+    Port ( clk : in STD_LOGIC;
+           reg_write : in STD_LOGIC;
+           imm7_op : in STD_LOGIC;
+           alu_op : in STD_LOGIC_VECTOR (1 downto 0);
+           instruction : std_logic_vector (15 downto 0);
+           a_equ_b : out STD_LOGIC);
+end component;
+
+signal clk, reg_write, imm7_op, a_equ_b : std_logic := '0';
+signal alu_op : std_logic_vector(1 downto 0) := (others => '0');
+signal instruction : std_logic_vector(15 downto 0) := (others => '0');
+
+begin
+
+dut : dp_alu_regfile port map (
+    clk => clk, reg_write => reg_write, imm7_op => imm7_op, alu_op => alu_op, instruction => instruction,
+    a_equ_b => a_equ_b
+);
+
+timing : process begin 
+    wait for 25ns;
+    clk <= not clk;
+end process timing;
+
+stimulus : process begin 
+    wait for 100ns; 
+    
+    -- addi r1, r0, 1
+    reg_write <= '1';
+    imm7_op <= '1';
+    alu_op <= "00";
+    instruction <= "001" & "001" & "000" & "0000001";
+    wait until clk = '1';
+    wait for 1ps;
+    
+    -- addi r2, r0, 2
+    instruction <= "001" & "010" & "000" & "0000010";
+    wait until clk = '1';
+    wait for 1ps;
+    
+    -- add r3, r2, r1 
+    instruction <= "001" & "011" & "010" & "0000" & "001";
+    
+    -- nand
+end process stimulus; 
+end Behavioral;
---
+++ risc16-single-cycle.srcs/sources_1/new/dp_alu_regfile.vhd
@@ -0,0 +1,117 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/11/2026 01:57:15 PM
+-- Design Name: 
+-- Module Name: dp_alu_regfile - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity dp_alu_regfile is
+    Port ( clk : in STD_LOGIC;
+           reg_write : in STD_LOGIC;
+           imm7_op : in STD_LOGIC;
+           alu_op : in STD_LOGIC_VECTOR (1 downto 0);
+           instruction : std_logic_vector (15 downto 0);
+           a_equ_b : out STD_LOGIC);
+end dp_alu_regfile;
+
+architecture Structural of dp_alu_regfile is
+
+component alu is
+    Port ( a : in STD_LOGIC_VECTOR (15 downto 0);
+           b : in STD_LOGIC_VECTOR (15 downto 0);
+           alu_op : in STD_LOGIC_VECTOR (1 downto 0);
+           c : out STD_LOGIC_VECTOR (15 downto 0);
+           a_equ_b : out STD_LOGIC);
+end component;
+
+component regfile is
+    Port ( clk : in STD_LOGIC;
+           reg_write : in STD_LOGIC;
+           a_addr : in STD_LOGIC_VECTOR (2 downto 0);
+           b_addr : in STD_LOGIC_VECTOR (2 downto 0);
+           c_addr : in STD_LOGIC_VECTOR (2 downto 0);
+           a_data : out STD_LOGIC_VECTOR (15 downto 0);
+           b_data : out STD_LOGIC_VECTOR (15 downto 0);
+           c_data : in STD_LOGIC_VECTOR (15 downto 0));
+end component;
+
+component extender2k is
+    Port ( data : in STD_LOGIC_VECTOR (6 downto 0);
+           q : out STD_LOGIC_VECTOR (15 downto 0));
+end component;
+
+component mux_2to1 is
+    generic ( data_width : integer := 16);
+    Port ( a : in std_logic_vector (data_width - 1 downto 0);
+           b : in std_logic_vector (data_width - 1 downto 0);
+           sel : in STD_LOGIC;
+           y : out std_logic_vector (data_width - 1 downto 0));
+end component;
+
+-- regfile
+signal a_addr, b_addr, c_addr : std_logic_vector(2 downto 0) := (others => '0');
+signal a_data, b_data, c_data : std_logic_vector (15 downto 0) := (others => '0');
+
+-- extender2k
+signal immediate : std_logic_vector(6 downto 0) := (others => '0');
+signal immediate16 : std_logic_vector (15 downto 0) := (others => '0');
+
+-- mux2to1 
+signal alu_b : std_logic_vector (15 downto 0) := (others => '0');
+
+begin
+
+c_addr <= instruction(12 downto 10);
+b_addr <= instruction(2 downto 0);
+a_addr <= instruction(9 downto 7);
+immediate <= instruction(6 downto 0);
+
+register_file : regfile port map (
+    clk => clk, reg_write => reg_write, 
+    a_addr => a_addr, b_addr => b_addr, c_addr => c_addr,
+    a_data => a_data, b_data => b_data, c_data => c_data
+);
+
+extender_2k : extender2k port map (
+    data => immediate,
+    q => immediate16
+);
+
+mux_b_imm : mux_2to1 port map (
+    a => b_data, b => immediate16,
+    sel => imm7_op, 
+    y => alu_b
+);
+
+alu16 : alu port map (
+    a => a_data, b => alu_b, alu_op => alu_op,
+    c => c_data, a_equ_b => a_equ_b
+);
+
+end Structural;
